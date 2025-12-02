8317463cac25899ca14d617bbce003fe8a8fc0f7
JozyMont <korolv900@gmail.com>
2025-12-03 00:02:15 +0200

    Added basic functional for the buttons in the screen 'Home'

--- front/DMarketAlert/app/src/main/java/com/example/dmarketalert/HomeFragment.kt
+++ front/DMarketAlert/app/src/main/java/com/example/dmarketalert/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.example.dmarketalert
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.core.app.NotificationCompat
 
 class HomeFragment : Fragment() {
 
@@ -29,22 +32,30 @@ class HomeFragment : Fragment() {
         checkAPI = view.findViewById(R.id.imageView_check_API)
 
         addTarget.setOnClickListener {
-            null
+            val intent1 = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://dmarket.com/ingame-items/item-list/csgo-skins?exchangeTab=myTargets"))
+            startActivity(intent1)
         }
 
         removeTarget.setOnClickListener {
-            null
+            val intent2 = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://dmarket.com/ingame-items/item-list/csgo-skins?exchangeTab=myTargets"))
+            startActivity(intent2)
         }
 
         updatePage.setOnClickListener {
             parentFragmentManager.beginTransaction()
-                .detach(this)
-                .attach(this)
+                .replace(R.id.fragment_container, HomeFragment())
                 .commit()
         }
 
         checkAPI.setOnClickListener {
-            null
+            val intent3 = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://dmarket.com/ru/ingame-items/item-list/csgo-skins?utm_source=google&utm_medium=cpc&utm_campaign=dm_new_brand-ua_s&gclid=Cj0KCQiAubrJBhCbARIsAHIdxD9iQgymE9e0Xf1OyoNVeUuAmbHiM7ecPEBO_oniDF8EqwVkkQVi-QsaAq1yEALw_wcB&cheapestBySteamAnalyst=true"))
+            startActivity(intent3)
         }
 
         return view
