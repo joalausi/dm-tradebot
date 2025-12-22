c3f6ac3ebfa96a00feb11249c0f642bfb28ee2a8
JozyMont <korolv900@gmail.com>
2025-12-22 22:02:46 +0200

    Added UI for two fragments, but without programiring

--- front/DMarketAlert/app/build.gradle.kts
+++ front/DMarketAlert/app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- front/DMarketAlert/app/src/main/java/com/example/dmarketalert/ProfileFragment.kt
+++ front/DMarketAlert/app/src/main/java/com/example/dmarketalert/ProfileFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        return view
     }
 }
